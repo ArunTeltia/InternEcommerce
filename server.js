2b6b2702646b8a65516511b9a9990352d78193e2
ArunTeltia <teltia.arun18@gmail.com>
2020-04-25 23:32:46 +0530

    ready for deploy

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const cors = require("cors");
 const authenticate = require("./api/middleware/authenticate");
-const path = require('path');
-mongoose.connect(
-  "mongodb+srv://arun1234:arun1234@hubx-1ftsz.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true },()=>{
-      console.log("mongodb running");
-  }
-);
+const path = require("path");
+const connectDB = require("./config/db");
+connectDB();
 const adminRoutes = require("./api/routes/admins");
 const categoryRoutes = require("./api/routes/categories");
 const userRoutes = require("./api/routes/users");
@@ -33,15 +28,15 @@ app.use((req, res, next) => {
 });
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 const port = process.env.PORT || 5000;
-app.listen(port,()=>{
-  console.log('server is running');
+app.listen(port, () => {
+  console.log("server is running");
 });
